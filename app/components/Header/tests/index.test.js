5587a2ccb9548869aa30b90e5bc10dd33924e7de
Evan Ristow <evan.ristow1@gmail.com>
2020-04-23 15:07:25 -0400

    Update/fix test.

--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -8,30 +8,45 @@
 
 import React from 'react';
 import { render } from 'react-testing-library';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router/immutable';
+import { createMemoryHistory } from 'history';
 // import 'jest-dom/extend-expect'; // add some helpful assertions
 
+import configureStore from '../../../configureStore';
 import Header from '../index';
 
 describe('<Header />', () => {
+  let history;
+  let store;
+
+  beforeAll(() => {
+    history = createMemoryHistory();
+    store = configureStore({}, history);
+  });
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    render(<Header />);
+    render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Header />
+        </ConnectedRouter>
+      </Provider>,
+    );
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
-    } = render(<Header />);
+    } = render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Header />
+        </ConnectedRouter>
+      </Provider>,
+    );
     expect(firstChild).toMatchSnapshot();
   });
 });
