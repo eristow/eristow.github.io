c264943d4fa337958f7722b970a7f9d465f1488b
Evan Ristow <evan.ristow1@gmail.com>
2020-04-28 14:42:15 -0400

    Make this repo the build/deploy repo

--- app/containers/Links/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for Links
- *
- */
-
-import loadable from 'utils/loadable';
-
-export default loadable(() => import('./index'));
--- app/containers/Stuff/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for Stuff
- *
- */
-
-import loadable from 'utils/loadable';
-
-export default loadable(() => import('./index'));
